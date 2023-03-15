4df1ecba78268ebaea106a38bda9c2e050b9c58a
Ayush Anshul <aanshul@adobe.com>
2023-03-16 00:08:37 +0530

    Connect to mongo db using mongoose and body parser shit I have to google

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
 
 const app = express();
 const PORT = 3000;
+
+
+// mongo connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send(`Our Blogging Application is running on port ${PORT}`)
